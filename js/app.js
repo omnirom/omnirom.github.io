7632c161dd2e5f5719e5f230b4039b7e9cee9fee
Vachounet <vachounet@live.fr>
2021-02-13 01:24:56 +0100

    devices: add buttons to switch between Q and R devices

--- js/app.js
+++ js/app.js
@@ -1,32 +1,28 @@
 import Router from './router.js'
 
 var navLinks = document.querySelectorAll(".nav-link");
-let currentRouter = Router.routes[window.location.hash];
-currentRouter.displayView();
-
-if (window.location.hash) {
-	manageActiveLink(window.location.hash)
-}
-
-navLinks.forEach(link => {
-	let destination = link.href
-	if (destination) {
-		link.onclick = function () {
-			manageActiveLink(destination)
-			let currentRouter = Router.routes[destination];
-			window.history.pushState(
-				{},
-				destination,
-				window.location.origin + destination
-			)
-			currentRouter.displayView()
-		}
-	}
-});
+handleRoute(window.location.hash)
 
 window.onpopstate = () => {
-	let currentRouter = Router.routes[window.location.hash];
-	currentRouter.displayView();
+	handleRoute(window.location.hash)
+	window.history.pushState(
+		{},
+		destination,
+		window.location.origin + window.location.hash
+	)
+}
+
+function handleRoute(destination) {
+	manageActiveLink(destination)
+	let currentRouter = Object.keys(Router.routes)
+		.filter(route =>
+			route !== "" && destination.includes(route))
+
+	if (destination.includes('/')) {
+		Router.routes[currentRouter].displayView(destination)
+	} else {
+		Router.routes[currentRouter].displayView()
+	}
 }
 
 function manageActiveLink(hash) {
