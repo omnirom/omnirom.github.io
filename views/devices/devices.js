7d8328c4d48e405ea2a22d14e52e987141525c64
maxwen <max.weninger@gmail.com>
2022-05-24 00:28:13 +0200

    devices: add optional readme button if in meta json
    Change-Id: I0b82a4fd9742b9ef78ecdd1000ede2f4d2815cee

--- views/devices/devices.js
+++ views/devices/devices.js
@@ -61,8 +61,22 @@ class DevicesView {
     var devicesContainer = tempObject.querySelector('#device-list')
 
     devicesList.forEach(device => {
-      const card = `
-      <div class="card device-card col-lg-3 col-md-4 col-sm-5">
+      if (device['readme']) {
+        const card = `
+        <div class="card device-card col-lg-3 col-md-4 col-sm-5">
+          <img src="${device['image']}" class="card-img-top" alt="${device['model']}" >
+          <div class="card-body">
+            <h5 class="card-title">${device['model']}</h5>
+            <p class="card-text">${device['make']}<br>${device['state']}</p>
+            <a href="${device['pageUrl']}" target="_blank" class="btn btn-omni">Download</a>
+            <a href="${device['changelog']}" target="_blank" class="btn btn-omni">Changelog</a>
+            <a href="${device['readme']}" target="_blank" class="btn btn-omni">Readme</a>
+          </div>
+        </div> `;
+        devicesContainer.innerHTML += card
+      } else {
+        const card = `
+        <div class="card device-card col-lg-3 col-md-4 col-sm-5">
           <img src="${device['image']}" class="card-img-top" alt="${device['model']}" >
           <div class="card-body">
             <h5 class="card-title">${device['model']}</h5>
@@ -71,7 +85,8 @@ class DevicesView {
             <a href="${device['changelog']}" target="_blank" class="btn btn-omni">Changelog</a>
           </div>
         </div> `;
-      devicesContainer.innerHTML += card
+        devicesContainer.innerHTML += card
+      }
     })
 
     let version = currentVersion.replace(/\./g, "_")
