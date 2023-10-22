af6ec9742ad145643e1e37fc11b13d08079b01ca
maxwen <max.weninger@gmail.com>
2023-10-22 21:50:06 +0200

    website: load supporters list from file

--- views/supporters/supporters.js
+++ views/supporters/supporters.js
@@ -1,16 +1,44 @@
 import { siteURL, container } from '../../js/const.js'
 
+const rawURL = 'https://raw.githubusercontent.com/omnirom/omnirom.github.io/master/config/supporters.txt'
+var supportersList = []
+
 class SupportersView {
 
   constructor() {
   }
 
+  async loadFromFile() {
+    try {
+      await axios.get(rawURL).then(function (response) {
+        supportersList = response.data.split(/\r?\n/)
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   async displayView() {
     try {
+      await this.loadFromFile()
       let pageContent = await axios
         .get(siteURL + 'views/supporters/supporters.html');
-      let post = await pageContent.data;
-      container.innerHTML = post
+      let supportersView = pageContent.data;
+      var tempObject = document.createElement('div');
+      tempObject.innerHTML = supportersView;
+      var devicesContainer = tempObject.querySelector('#supporters-list')
+
+      supportersList.forEach(supporter => {
+        if (supporter.length != 0) {
+          const row = `
+        <li>
+          ${supporter}
+        </li> `;
+          devicesContainer.innerHTML += row
+
+        }
+      })
+      container.innerHTML = tempObject.innerHTML
     } catch (error) {
       console.log("display supporters view error: " + error);
     }
